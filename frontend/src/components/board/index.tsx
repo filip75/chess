f60a919aa708f34a7934fdd639d6dc5ac627d8a2
Filip Janowski <filip.janowski@cluepoints.com>
2021-04-07 21:19:59 +0200

    [FE] some cleanup

--- frontend/src/components/board/index.tsx
+++ frontend/src/components/board/index.tsx
@@ -2,54 +2,95 @@ import React, { useReducer } from 'react'
 import { range } from '../../missing'
 import { Colour, PieceType } from '../../model/piece'
 import FieldComponent from '../fieldComponent'
-import { Field } from '../interface'
+import { Field, Piece } from '../interface'
 import styles from './style.module.scss'
 
-const placePiece = (
-    field: Field,
-    pieceType: PieceType,
-    pieceColour: Colour
-): Field => {
+const placePiece = (field: Field, piece: Piece): Field => {
     return {
         ...field,
-        pieceType,
-        pieceColour,
+        piece,
     }
 }
 
 const prepareBoard = (): BoardState => {
     const fields = range(64).map(
         (): Field => ({
-            pieceType: null,
-            pieceColour: null,
-            marked: false,
+            piece: null,
             possible: false,
         })
     )
 
-    fields[0] = placePiece(fields[0], PieceType.Rook, Colour.Black)
-    fields[1] = placePiece(fields[1], PieceType.Bishop, Colour.Black)
-    fields[2] = placePiece(fields[2], PieceType.Knight, Colour.Black)
-    fields[3] = placePiece(fields[3], PieceType.Queen, Colour.Black)
-    fields[4] = placePiece(fields[4], PieceType.King, Colour.Black)
-    fields[5] = placePiece(fields[5], PieceType.Knight, Colour.Black)
-    fields[6] = placePiece(fields[6], PieceType.Bishop, Colour.Black)
-    fields[7] = placePiece(fields[7], PieceType.Rook, Colour.Black)
-    fields[8] = placePiece(fields[8], PieceType.Pawn, Colour.Black)
-    fields[9] = placePiece(fields[9], PieceType.Pawn, Colour.Black)
-    fields[10] = placePiece(fields[10], PieceType.Pawn, Colour.Black)
-    fields[11] = placePiece(fields[11], PieceType.Pawn, Colour.Black)
-    fields[12] = placePiece(fields[12], PieceType.Pawn, Colour.Black)
-    fields[13] = placePiece(fields[13], PieceType.Pawn, Colour.Black)
-    fields[14] = placePiece(fields[14], PieceType.Pawn, Colour.Black)
-    fields[15] = placePiece(fields[15], PieceType.Pawn, Colour.Black)
+    fields[0] = placePiece(fields[0], {
+        type: PieceType.Rook,
+        colour: Colour.Black,
+    })
+    fields[1] = placePiece(fields[1], {
+        type: PieceType.Bishop,
+        colour: Colour.Black,
+    })
+    fields[2] = placePiece(fields[2], {
+        type: PieceType.Knight,
+        colour: Colour.Black,
+    })
+    fields[3] = placePiece(fields[3], {
+        type: PieceType.Queen,
+        colour: Colour.Black,
+    })
+    fields[4] = placePiece(fields[4], {
+        type: PieceType.King,
+        colour: Colour.Black,
+    })
+    fields[5] = placePiece(fields[5], {
+        type: PieceType.Knight,
+        colour: Colour.Black,
+    })
+    fields[6] = placePiece(fields[6], {
+        type: PieceType.Bishop,
+        colour: Colour.Black,
+    })
+    fields[7] = placePiece(fields[7], {
+        type: PieceType.Rook,
+        colour: Colour.Black,
+    })
+    fields[8] = placePiece(fields[8], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
+    fields[9] = placePiece(fields[9], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
+    fields[10] = placePiece(fields[10], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
+    fields[11] = placePiece(fields[11], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
+    fields[12] = placePiece(fields[12], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
+    fields[13] = placePiece(fields[13], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
+    fields[14] = placePiece(fields[14], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
+    fields[15] = placePiece(fields[15], {
+        type: PieceType.Pawn,
+        colour: Colour.Black,
+    })
 
-    return { fields, marked: false }
+    return { fields, marked: null }
 }
 
 interface BoardState {
     fields: Field[]
-    marked: boolean
+    marked: number | null
 }
 
 interface ActionType {
@@ -60,13 +101,13 @@ interface ActionType {
 const boardReducer = (state: BoardState, action: ActionType): BoardState => {
     switch (action.type) {
         case 'MARK': {
+            const index: number = action.payload.index
             const f: Field = {
                 ...state.fields[action.payload.index],
-                marked: true,
             }
             const fields = [...state.fields]
             fields[action.payload.index] = f
-            return { ...state, fields }
+            return { ...state, fields, marked: index }
         }
 
         default:
@@ -84,9 +125,8 @@ const Board: React.FC = (): React.ReactElement => {
                     <FieldComponent
                         key={n}
                         index={n}
-                        pieceType={board.fields[n].pieceType}
-                        pieceColour={board.fields[n].pieceColour}
-                        marked={board.fields[n].marked}
+                        piece={board.fields[n].piece}
+                        marked={board.marked ? board.marked === n : false}
                         onClick={(index: number) =>
                             boardDispatch({ type: 'MARK', payload: { index } })
                         }
